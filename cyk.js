54231846f49c0ee8cf94ef89f9e1f66c1ffe64c9
Philip Rosen <philipjrosen@gmail.com>
2015-01-20 11:53:38 -0800

    adds makeParent and toy cnfGrammar

--- cyk.js
+++ cyk.js
@@ -1,1 +1,24 @@
 var pos = require('pos');
+
+var cnfGrammar = {
+
+  "S" : "NP VP",
+  "NP" : "DT NN",
+  "VP" : "VB NP",
+  "DET" : "the",
+  "VB" : "chased",
+  "NN" : "cat",
+  "NN" : "dog"
+};
+
+
+var makeParent = function(name) {
+  var parent = {};
+  parent.children = [];
+  parent.name = name;
+  return parent
+};
+
+
+exports.makeParent = makeParent;
+exports.cnfGrammar = cnfGrammar;
